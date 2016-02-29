92b43718ddfa2d72d0e2d513ced3dac6e2879ee8
nrathore <nalini.rathore@gmail.com>
2016-02-29 03:36:51 -0500

    NR Programming Assigment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## creates a special vector - 
+## a list containing a function to set and get a matrix, and set and get mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+m = NULL
+set = function(y) {
+  x <<- y
+  m <<- NULL
+}
+get = function() x
+setinv = function(solve) m <<- solve
+getinv = function() m
+list(set = set, get = get, 
+     setinv = setinv, 
+     getinv = getinv)
 }
 
 
@@ -12,4 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mtx = x$getinv()
+    if(!is.null(mtx)) {
+      message("getting cached data")
+      return(mtx)
+    }
+    data = x$get()
+    mtx = solve(data, ...)
+    x$setinv(mtx)
+    return(mtx)
 }
+
+
